ee0eb9d1c1d30b55d43cdb4a2891c616a9086b68
Aladnane <56963120+Aladnane@users.noreply.github.com>
2021-09-28 12:24:57 +0100

    m2

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-    if($_GET("username") == 'aladnane' && $_GET("password") == '1234')
-    {
+    /*if($_GET("username") == 'aladnane' && $_GET("password") == '1234')
+    {*/
         echo "Correct";
-    }
+    /*}
     else
-        echo "Incorrect";
+        echo "Incorrect";*/
